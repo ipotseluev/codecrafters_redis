584d9919aabe7c6eac8a61b57d49b69dd74b01c5
Igor Potseluev <4eppelin@gmail.com>
2024-02-26 12:59:49 +0200

    Implement pong

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,29 @@
-use std::net::TcpListener;
+use std::error::Error;
+use std::io::{BufRead, BufReader, Read, Write};
+use std::net::{TcpListener, TcpStream};
 
-fn main() {
+fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
+    println!("handling connection");
+    let buf = &mut String::new();
+    {
+        let mut buf_reader = BufReader::new(&stream).take(1000);
+        while let Ok(req_size) = buf_reader.read_line(buf) {
+            println!("Got {} bytes: '{}'", req_size, buf.trim());
+            if buf.trim() == "ping" {
+                println!("Got ping");
+                break;
+            }
+            buf.clear();
+        }
+    }
+    let pong = b"+PONG\r\n";
+    println!("Answering with: pong");
+    stream.write_all(pong)?;
+
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
@@ -8,12 +31,16 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
+                handle_connection(stream)?;
             }
             Err(e) => {
                 println!("error: {}", e);
+                return Err(e.into());
             }
         }
     }
+
+    Ok(())
 }
